7b41a147279a59c67c436d2d518a10b56a5ed53c
MadhushankaUCSC <buthsaramadhushanka@gmail.com>
2020-11-03 10:59:26 +0530

    add repeatnumber file

---
+++ Basic java projects/RepeatNumber.java
@@ -0,0 +1,27 @@
+public class RepeatNumber
+
+
+
+{
+
+    public static void main(String args[])
+
+	{
+
+		int number=0;
+
+		while (number<11)
+
+		  { 
+			System.out.println(number);
+			number =number +1;
+		
+		  }
+
+		
+		
+
+	}
+
+
+}	
