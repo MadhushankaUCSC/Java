df2d31325ad83fca0a1dbdcf1d8aba3d44ba203b
MadhushankaUCSC <buthsaramadhushanka@gmail.com>
2020-11-03 10:57:54 +0530

    add logical test file

---
+++ Basic java projects/LogicalTest.java
@@ -0,0 +1,21 @@
+public class LogicalTest
+
+
+{
+	public static void main(String args[])
+
+	{
+		int a=10;
+		int b=20;
+	        
+		if(a>b)
+		  System.out.println("This is true");
+	
+		else
+		  System.out.println("This is false");
+
+
+
+	}
+
+}	
